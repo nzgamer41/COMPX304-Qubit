93d5ca9ff6456e881f684147eb8d04b37bd7008e
nzgamer41 <pslover9@gmail.com>
2020-10-21 01:01:34 +1300

    added mitm explanations

--- COMPX304-Qubit/COMPX304-Qubit.Code/Program.cs
+++ COMPX304-Qubit/COMPX304-Qubit.Code/Program.cs
@@ -14,13 +14,13 @@ namespace COMPX304_Qubit
             int msgLength = 20;
             // once i'm sure on the enc/dec stuff i will write some basic networking support for transmit/recv but for now the transmitter will store their qubits in here and the receiver will store theirs in another list.
             List<Qubit> pretendTransmission = new List<Qubit>();
-            List<Qubit> pretendReceiver = new List<Qubit>();
+            
             //
             Random random = new Random();
             StringBuilder sb;
 
             //message commands are written to debug so the message is hidden from end user atm (mostly having fun with this)
-            Debug.WriteLine("- Generating message.....");
+            Console.WriteLine("- [DEBUG] Generating message.....");
             byte[] msg = new byte[msgLength];
             random.NextBytes(msg);
             sb = new StringBuilder();
@@ -29,7 +29,7 @@ namespace COMPX304_Qubit
                 sb.Append(b);
             }
             string msgString = sb.ToString();
-            Debug.WriteLine("- Generated!\n- The message to be encrypted is " + msgString);
+            Console.WriteLine("- [DEBUG] Generated!\n- [DEBUG] The message to be encrypted is " + msgString);
 
             //now comes the fun times
             Console.WriteLine("- [TRANSMITTER] Generating qubits...");
@@ -74,8 +74,29 @@ namespace COMPX304_Qubit
                 receiverPol.Add(pol);
             }
 
+            Console.WriteLine("- [FAKE NETWORKING, NO PACKETS SENT] [TRANSMITTER] Exchanging polarizations...");
 
-            Console.WriteLine("- [ALL] Comparing polarization types and forming key...");
+            // Because I haven't written any actual message sending, this sorta demonstrates how the MITM attack would actually work.
+            //Pretend that here, the packets have been sniffed between machines
+            StringBuilder pol1 = new StringBuilder();
+            foreach (int i in transmitPol)
+            {
+                pol1.Append(i);
+            }
+            Console.WriteLine("- [FAKE NETWORKING, NO PACKETS SENT] [MITM] 1st set of polarizations are: " + pol1.ToString());
+
+
+
+            Console.WriteLine("- [FAKE NETWORKING, NO PACKETS SENT] [RECEIVER] Exchanging polarizations...");
+            StringBuilder pol2 = new StringBuilder();
+            foreach (int i in receiverPol)
+            {
+                pol2.Append(i);
+            }
+            Console.WriteLine("- [FAKE NETWORKING, NO PACKETS SENT] [MITM] 2nd set of polarizations are: " + pol2.ToString());
+
+
+            Console.WriteLine("- [TRNSMT/RECV] Comparing polarization types and forming key...");
 
             StringBuilder secretKey = new StringBuilder();
             for (int i = 0; i < receiverPol.Count; i++)
@@ -85,6 +106,8 @@ namespace COMPX304_Qubit
                     secretKey.Append(pretendTransmission[i].measure(transmitPol[i]));
                 }
             }
+
+            Console.WriteLine("- [MITM] In a real scenario, a man-in-the-middle attack would sniff the packets going each way with the polarizations as well as the Qubits themselves, then do the exact same comparison.\n- [MITM] Because there's no actual packets being sent, the key being printed twice is a waste of time.");
 
             Console.WriteLine("- [ALL] The secret key is: " + secretKey.ToString());
 
